cb26b73f25df4bab824965f93d4157a157415088
Scott Kuroda <scott.kuroda@gmail.com>
2013-02-20 19:44:47 -0800

    Add command to open packages and allow editing

--- list_package_files.py
+++ list_package_files.py
@@ -1,14 +1,17 @@
 import sublime
 import sublime_plugin
 import re
-from ListPackageFiles.package_resources.package_resources import *
+from PackageResourceViewer.package_resources.package_resources import *
+
+VERSION = sublime.version()
+PACKAGES_PATH = sublime.packages_path()
 
 
-class ListPackageFilesCommand(sublime_plugin.WindowCommand):
+class PackageResourceViewerBase(sublime_plugin.WindowCommand):
     def run(self):
         self.packages = get_packages_list()
         self.show_quick_panel(self.packages, self.package_list_callback)
-        self.settings = sublime.load_settings("ListPackageFiles.sublime-settings") 
+        self.settings = sublime.load_settings("ListPackageFiles.sublime-settings")
 
     def package_list_callback(self, index):
         if index == -1:
@@ -20,20 +23,47 @@ class ListPackageFilesCommand(sublime_plugin.WindowCommand):
         self.show_quick_panel(self.files, self.package_file_callback)
 
     def package_file_callback(self, index):
+        raise "Should be implemented by child class"
+
+    def set_read_only(self, read_only):
+        sublime.set_timeout(lambda: self.window.active_view().set_read_only(read_only), 10)
+
+    def show_quick_panel(self, options, done_callback):
+        sublime.set_timeout(lambda: self.window.show_quick_panel(options, done_callback), 10)
+
+
+class ViewPackageFileCommand(PackageResourceViewerBase):
+    def package_file_callback(self, index):
         if index == -1:
             return
         entry = self.files[index]
         if entry == "..":
             self.show_quick_panel(self.packages, self.package_list_callback)
         else:
-            self.window.run_command("open_file", {"file": "${packages}/" + self.package + "/" + entry})
+            self.window.run_command("open_file", {"file": "${packages}/" + package_file})
             if self.package != "User" and self.settings.get("read_only_non_user", True):
-                self.set_read_only()
+                self.set_read_only(True)
             if self.settings.get("open_multiple", False):
                 self.show_quick_panel(self.files, self.package_file_callback)
 
-    def set_read_only(self):
-        sublime.set_timeout(lambda: self.window.active_view().set_read_only(True), 10)
 
-    def show_quick_panel(self, options, done_callback):
-        sublime.set_timeout(lambda: self.window.show_quick_panel(options, done_callback), 10)
+class EditPackageFileCommand(PackageResourceViewerBase):
+    def package_file_callback(self, index):
+        if index == -1:
+            return
+        entry = self.files[index]
+        if entry == "..":
+            self.show_quick_panel(self.packages, self.package_list_callback)
+        else:
+            self.create_folder(os.path.join(sublime.packages_path(), self.package))
+            self.window.run_command("open_file", {"file": "${packages}/" + self.package + "/" + entry})
+            self.set_read_only(False)
+            if self.settings.get("open_multiple", False):
+                self.show_quick_panel(self.files, self.package_file_callback)
+
+    def create_folder(self, base):
+        if not os.path.exists(base):
+            parent = os.path.split(base)[0]
+            if not os.path.exists(parent):
+                self.create_folder(parent)
+            os.mkdir(base)
